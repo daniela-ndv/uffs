abb52bc192f79b9ec016f1bd8248f2066863be01
Daniela Dalla Vecchia <114515986+daniela-ndv@users.noreply.github.com>
2026-04-20 00:13:00 -0300

    Add constante RESP-HTTP e pequenas correções (#2)

--- Prog-II/src/controllers/alunoController.js
+++ Prog-II/src/controllers/alunoController.js
@@ -2,48 +2,49 @@
 // Não contém lógica de negócio nem queries SQL.
 
 const AlunoModel = require('../models/alunoModel');
+const RESP_HTTP = require('../../consts');
 
 function listar(req, res) {
     const alunos = AlunoModel.listarTodos(req.query);
-    res.status(200).json({ total: alunos.length, alunos });
+    res.status(RESP_HTTP.OK).json({ total: alunos.length, alunos });
 }
 
 function buscar(req, res) {
     const id = parseInt(req.params.id);
-    if (isNaN(id)) return res.status(400).json({ erro: 'ID inválido' });
+    if (isNaN(id)) return res.status(RESP_HTTP.BAD_REQUEST).json({ erro: 'ID inválido' });
     const aluno = AlunoModel.buscarPorId(id);
-    if (!aluno) return res.status(404).json({ erro: 'Aluno não encontrado' });
-    res.status(200).json(aluno);
+    if (!aluno) return res.status(RESP_HTTP.NOT_FOUND).json({ erro: 'Aluno não encontrado' });
+    res.status(RESP_HTTP.OK).json(aluno);
 }
 
 function criar(req, res) {
     try {
         const novoAluno = AlunoModel.criar(req.body);
-        res.status(201).set('Location', '/api/alunos/' + novoAluno.id).json(novoAluno);
+        res.status(RESP_HTTP.CREATED).set('Location', '/api/alunos/' + novoAluno.id).json(novoAluno);
     } catch (err) {
-        res.status(400).json({ erro: err.message });
+        res.status(RESP_HTTP.BAD_REQUEST).json({ erro: err.message });
     }
 }
 
 function atualizar(req, res) {
     const id = parseInt(req.params.id);
     const aluno = AlunoModel.atualizar(id, req.body);
-    if (!aluno) return res.status(404).json({ erro: 'Aluno não encontrado' });
-    res.status(200).json(aluno);
+    if (!aluno) return res.status(RESP_HTTP.NOT_FOUND).json({ erro: 'Aluno não encontrado' });
+    res.status(RESP_HTTP.OK).json(aluno);
 }
 
 function atualizarParcial(req, res) {
     const id = parseInt(req.params.id);
     const aluno = AlunoModel.atualizarParcial(id, req.body);
-    if (!aluno) return res.status(404).json({ erro: 'Aluno não encontrado' });
-    res.status(200).json(aluno);
+    if (!aluno) return res.status(RESP_HTTP.NOT_FOUND).json({ erro: 'Aluno não encontrado' });
+    res.status(RESP_HTTP.OK).json(aluno);
 }
 
 function remover(req, res) {
     const id = parseInt(req.params.id);
     const ok = AlunoModel.remover(id);
-    if (!ok) return res.status(404).json({ erro: 'Aluno não encontrado' });
-    res.status(204).send();
+    if (!ok) return res.status(RESP_HTTP.NOT_FOUND).json({ erro: 'Aluno não encontrado' });
+    res.status(RESP_HTTP.NO_CONTENT).send();
 }
 
 module.exports = { listar, buscar, criar, atualizar, atualizarParcial, remover };
